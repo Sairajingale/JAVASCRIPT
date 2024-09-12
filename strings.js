1f22c63606fbd4cb71ae84f745e3d800a2a664d3
Sairaj Ingale <114092602+Sairajingale@users.noreply.github.com>
2024-09-12 13:29:50 +0000

    strings

--- strings.js
+++ strings.js
@@ -3,3 +3,23 @@ let Cityname = "Mumbai";
 const AnotherCityName = CityName;
 
 console.log(AnotherCityName)
+
+const Myname = "Sairaj";
+const Amount = 100;
+
+console.log (`My ${Myname} is and I have ${Amount} rupees`);
+
+
+let Name = new  String("adminstration");
+
+console.log(Name.slice(0,4))
+console.log(Name[1])
+console.log(Name.trim())
+console.log(Name.charAt(3))
+console.log(Name.indexOf("s"))
+
+
+const sentence = "Hello world, how are you?";
+const words = sentence.split(' ');
+console.log(words);
+// Output: ["Hello", "world,", "how", "are", "you?"]
