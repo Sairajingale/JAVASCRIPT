c7a63b54007b7fead304897c44a4ecb2d64041de
Sairaj Ingale <114092602+Sairajingale@users.noreply.github.com>
2024-09-12 14:33:27 +0000

    Strings

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 let Cityname = "Mumbai";
 
-const AnotherCityName = CityName;
+const AnotherCityName = Cityname;
 
 console.log(AnotherCityName)
 
@@ -17,7 +17,10 @@ console.log(Name[1])
 console.log(Name.trim())
 console.log(Name.charAt(3))
 console.log(Name.indexOf("s"))
+console.log(Name.toUpperCase())
 
+let url = "https://www.google.com/hello%20world%20and%20hello%20universe";
+console.log( url.replace("%20","-")  )
 
 const sentence = "Hello world, how are you?";
 const words = sentence.split(' ');
