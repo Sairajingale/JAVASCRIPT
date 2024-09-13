daeef4b1a37f3c6ab876724278f7760102889e2f
Sairaj Ingale <114092602+Sairajingale@users.noreply.github.com>
2024-09-13 06:52:24 +0000

    number And math functions

---
+++ 01_Basics/3_Number.js
@@ -0,0 +1,32 @@
+// let num= 400;
+
+// const num1 = new Number (500.26)
+
+// console.log(typeof num)
+// console.log(num1)
+// console.log(typeof num1)
+
+// console.log(typeof num1.toString().length);
+
+// console.log(num1.toFixed(2));
+
+// console.log(num1.toPrecision(4));
+
+
+//--------------------------------------------------//
+// const anynum = 5000.400
+// console.log(Math.random())
+// console.log(Math.round(anynum))
+// console.log(Math.abs(anynum))
+// console.log(Math.floor(anynum))
+// console.log(Math.ceil(anynum))
+// console.log(Math.min(anynum))
+// console.log(Math.max(anynum))
+
+
+// Ludo Dice 
+
+const max = 6;
+const min = 1;
+
+console.log(Math.round(Math.random() * ( max - min)+ min))
