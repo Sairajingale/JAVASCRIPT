456ed057fbe74d41884f025043dc26adfb047d2f
Sairaj Ingale <sairaj.1ingale@gmail.com>
2025-02-13 04:35:46 +0530

    practice of functions

--- 03_Basics/8_functions.js
+++ 03_Basics/8_functions.js
@@ -2,4 +2,48 @@ function addTwoNumbers(num1, num2){
     return num1 + num2;
 }
 
-addTwoNumbers(2,3);
+console.log(addTwoNumbers(2,3));
+
+function username(user){
+    return `${user} is logged in`;
+}
+
+console.log(username("John"));
+
+function password(password){
+if(password===undefined)
+    return "Please enter a password";
+
+else{
+     return "Password is set";
+}
+}
+
+console.log(password()); // Please enter a password
+
+
+//object passing 
+
+obj = {
+    product: "Iphone",
+    price: 80000
+};
+
+function productDetails(obj){
+    return `${obj.product} price is ${obj.price}`;
+}
+
+console.log(productDetails(obj)); // Iphone price is 1000
+console.log(productDetails({product:"Samsung",price:125000})); 
+
+
+//Array passing
+
+products = ["Iphone","Samsung","Oneplus"];
+
+function productName(products){
+    return `${products[0]} is the best smartphone brand`;
+}
+
+console.log(productName(products)); // Iphone is the best smartphone brand
+console.log(productName(['oppo'])); 
